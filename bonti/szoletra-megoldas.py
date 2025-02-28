81d8c99bff887970d64dc800c7a7e751eb139aca
brgszszlvSuli <beregszaszi.levi.suli@gmail.com>
2025-02-28 11:24:19 +0100

    Add files via upload

--- bonti/szoletra-megoldas.py
+++ bonti/szoletra-megoldas.py
@@ -3,7 +3,7 @@ print("Milyen hosszú legyen a szó?")
 hossz = int(input())  # 4
 print("Kérem a szót!")
 elozoszo = input()  # alma
-
+pont = 0
 while True:
     if len(elozoszo) != hossz:
         print(f"Hosszhiba!")
@@ -14,4 +14,13 @@ while True:
         print("Létrahiba")
         break
     elozoszo = kovetkezoszo
+    pont += 1
 print("Játék vége")
+
+print(pont)
+if 0 >= pont <= 3:
+    print("Alap")
+elif 4 >= pont <= 10:
+    print("Közép")
+else:
+    print("Profi")
